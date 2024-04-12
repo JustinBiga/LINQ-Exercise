c4c734848d0d1932b99da6006c71a7e41a0a9c66
JustinBiga <BISENGOJ@GMAIL.COM>
2024-04-12 10:10:25 -0400

    Init commit

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videoGames = new List<string>
+            {
+                "Red Dead Redemption 2",
+                "The Witcher 3: Wild Hunt",
+                "Dark Souls III",
+                "Super Mario Odyssey",
+                "Bloodborne",
+                "Persona 5",
+                "God of War",
+                "Hollow Knight",
+                "Sekiro: Shadows Die Twice",
+                "Grand Theft Auto V",
+                "The Legend of Zelda: A Link to the Past",
+                "Half-Life 2"
+
+            };
+            var gameCollection = videoGames.OrderBy(game => game.Length);
+
+            foreach(var item in gameCollection)
+            {
+                Console.WriteLine(item);
+            }
+
+           
         }
     }
 }
